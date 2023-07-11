30e50bcf8b1c3736e01ca3f41ad6a492c8e0aa9f
Your Name <you@example.com>
2023-07-11 09:18:48 -0400

    trying to fix loading

--- build_dataset.py
+++ build_dataset.py
@@ -7,11 +7,53 @@ load_from_disk instead of a named source
 """
 
 from glob import glob
-from datasets import Dataset , load_dataset, load_from_disk, Audio
+from datasets import Dataset, DatasetDict , load_dataset, load_from_disk, Audio
+import librosa 
+from uuid import uuid4
+from scipy.io import wavfile
+from tqdm import tqdm 
+import shutil 
+import os
+import random
 
-RAW_WAV = "raw_wav"
-DEST_DATASET='audio_dataset'
-fnames = glob(f"{RAW_WAV}/*")
-audio_dataset = Dataset.from_dict({"audio":fnames}).cast_column("audio", Audio())
-audio_dataset[0]["audio"]
-audio_dataset.save_to_disk(f"DEST_DATASET")
+N = 50   # number of files
+print("processing {} files".format(N))
+
+SECONDS = 15
+RAW_WAV = "/mnt/md0/raw_wavs"   # output directory for segments
+NEW_DATASET = "NEW_DATASET"
+fnames = glob("/mnt/md0/birdsong-recognition/train_audio/**/*mp3")
+random.shuffle(fnames) 
+
+try:
+	shutil.rmtree(RAW_WAV)
+except:
+	pass
+
+os.mkdir(RAW_WAV)
+os.mkdir(f"{RAW_WAV}/clean")
+os.mkdir(f"{RAW_WAV}/clean/train")
+os.mkdir(f"{RAW_WAV}/clean/test")
+
+for f in tqdm(fnames[:N]):
+	x, sr = librosa.load(f, sr=16000)
+#	print("processing file {}".format(f))
+
+	for idx in range(0, x.shape[0]-16000*SECONDS, 8000*SECONDS):
+		y = x[idx:idx+16000]
+		if(random.random()<0.9):
+			dest ="train"
+		else:
+			dest="test"
+		fn = f"{RAW_WAV}/clean/{dest}/{str(uuid4())}.wav"
+		wavfile.write(fn, 16000, y)
+
+print("done exporting")
+fnames = glob(RAW_WAV+"/clean/**/*wav")
+assert len(fnames)>0
+print('creating dataset')
+audio_dataset = Dataset.from_dict({"file":fnames, "audio":fnames}).cast_column("audio", Audio())
+
+full_dataset  = DatasetDict({"train":audio_dataset})
+full_dataset.save_to_disk(NEW_DATASET)
+#audio_dataset.save_to_disk(NEW_DATASET)
